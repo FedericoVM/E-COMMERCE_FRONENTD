d3895b338c5d267a5dbf4ff8fcf53ba0f4627988
Federico Valdiglesias <federicomrtl@gmail.com>
2022-12-20 18:08:51 -0300

    Se realizo una correcion del json server

--- src/components/routers/RouterPrincipal.jsx
+++ src/components/routers/RouterPrincipal.jsx
@@ -1,10 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+
 export const RouterPrincipal = () => {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    const url = process.env.VITE_API_URL;
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getApi();
+    }, []);
+
+    const getApi = async () => {
+        try {
+            const resp = await fetch(url);
+            const productosApi = await resp.json();
+            setProducts(productosApi);
+        } catch (error) {
+            console.log(error);
+        }
+
+
+
+
+
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route/>
+                </Routes>
+            </BrowserRouter>
+        );
+    };
+}

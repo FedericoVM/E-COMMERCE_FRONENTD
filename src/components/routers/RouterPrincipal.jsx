4f5a5b68b42a026e40dd78dd9089515154f0ada4
Federico Valdiglesias <federicomrtl@gmail.com>
2022-12-21 10:10:07 -0300

    Se realizo el front de la pagina de destacados

--- src/components/routers/RouterPrincipal.jsx
+++ src/components/routers/RouterPrincipal.jsx
@@ -1,36 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-
+import Destacados from "../views/Destacados/Destacados";
+import DestacadosTest from "../views/Destacados/DestacadosTest";
 
 export const RouterPrincipal = () => {
+  const url = "http://localhost:3001/productos";
 
-    const url = process.env.VITE_API_URL;
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getApi();
+  }, []);
 
-    useEffect(() => {
-        getApi();
-    }, []);
-
-    const getApi = async () => {
-        try {
-            const resp = await fetch(url);
-            const productosApi = await resp.json();
-            setProducts(productosApi);
-        } catch (error) {
-            console.log(error);
-        }
-
-
-
-
-
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route/>
-                </Routes>
-            </BrowserRouter>
-        );
-    };
-}
+  const getApi = async () => {
+    try {
+      const resp = await fetch(url);
+      const productosApi = await resp.json();
+      setProducts(productosApi);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/destacados" element={<Destacados />} />
+          <Route exact path="/destacados-test" element={<DestacadosTest />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  
+};

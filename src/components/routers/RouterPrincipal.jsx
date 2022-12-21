02156beec3ab20d8d4e96afd6803f271b8374ad5
Federico Valdiglesias <federicomrtl@gmail.com>
2022-12-21 09:52:26 -0300

    Se realizo el front de la pagina de categorias

--- src/components/routers/RouterPrincipal.jsx
+++ src/components/routers/RouterPrincipal.jsx
@@ -1,36 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-
+import Electrodomesticos from "../views/Categorias/Electrodomesticos";
+import Computacion from "../views/Categorias/computacion/Computacion";
+import AireLibre from "../views/Categorias/aireLibre/AireLibre";
 
 export const RouterPrincipal = () => {
+  const url = "http://localhost:3001/productos";
 
-    const url = process.env.VITE_API_URL;
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getApi();
+  }, []);
 
-    useEffect(() => {
-        getApi();
-    }, []);
+  const getApi = async () => {
+    try {
+      const resp = await fetch(url);
+      const productosApi = await resp.json();
+      setProducts(productosApi);
+    } catch (error) {
+      console.log(error);
+    }
 
-    const getApi = async () => {
-        try {
-            const resp = await fetch(url);
-            const productosApi = await resp.json();
-            setProducts(productosApi);
-        } catch (error) {
-            console.log(error);
-        }
+  };
 
-
-
-
-
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route/>
-                </Routes>
-            </BrowserRouter>
-        );
-    };
-}
+    return (
+      <BrowserRouter>
+      <Routes>
+          <Route  path='/electrodomesticos' element={<Electrodomesticos/>}/>
+          <Route  path='/computacion' element={<Computacion/>}/>
+          <Route  path='/aire-libre' element={<AireLibre/>}/>
+      </Routes>
+      </BrowserRouter>
+    );
+  
+};

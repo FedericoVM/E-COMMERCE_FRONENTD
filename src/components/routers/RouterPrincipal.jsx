7ef9154bbd11382dec6b8795141e24c731b0c112
Federico Valdiglesias <78738426+FedericoVM@users.noreply.github.com>
2022-12-21 11:48:06 -0300

    Se realizo el front del login (#4)

--- src/components/routers/RouterPrincipal.jsx
+++ src/components/routers/RouterPrincipal.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-
+import Login from "../views/Login/Login";
 import Favoritos from "../views/Favoritos/Favoritos";
 import Contacto from "../views/Contacto/Contacto";
 import Destacados from "../views/Destacados/Destacados";
 import DestacadosTest from "../views/Destacados/DestacadosTest";
-
 
 export const RouterPrincipal = () => {
   const url = "http://localhost:3001/productos";
@@ -29,6 +28,7 @@ export const RouterPrincipal = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/" element={<Favoritos/>}/>
         <Route path="/" element={<Contacto />} />
       </Routes>

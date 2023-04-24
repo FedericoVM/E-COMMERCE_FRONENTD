b4b17128a462cca6e51e32eb05b1f8b3e7aa82b0
Federico Valdiglesias <78738426+FedericoVM@users.noreply.github.com>
2023-04-24 20:22:33 -0300

    Home (#20)
    * Se creo la pagina de home
    
    * Correcion en el nombre de la carpeta view

---
+++ src/components/views/home/productCard/ProductCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Card, Col, Nav, NavLink, Row } from "react-bootstrap";
+import { BsSuitHeartFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "./productCard.css";
+
+const ProductCard = ({ p}) => {
+  return (
+    <Card  className="card shadow-lg border m-sx-3 ">
+      <div>
+        <Card.Img
+          className="card-img border-bottom img-fluid"
+          variant="top"
+          src={p.imagen}
+        />
+        <Card.Title className="text-white title-precio rounded">
+          ${p.precio}
+        </Card.Title>
+        <Button variant="primary" className="boton-favorito">
+          <BsSuitHeartFill />
+        </Button>
+      </div>
+      <Card.Body className="h-100 d-flex flex-column ">
+        <div>
+          <Link to="/product" className="text-decoration-none">
+            <div className="d-flex justify-content-between container p-0 text-center  h-100">
+              <Card.Title className="text-dark ">{p.titulo}</Card.Title>
+            </div>
+          </Link>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCard;

40e578ca36e72da78c574bc860cfaf16e5831470
Federico Valdiglesias <federicomrtl@gmail.com>
2023-06-08 18:47:42 -0300

    Se creo el componente para mostrar la  informacion  de usuario y para editar

---
+++ src/components/views/cuentaUsuario/CuentaUsuario.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button, Col, Form, Image } from 'react-bootstrap'
+import "./cuentaUsuario.css"
+
+
+const CuentaUsuario = () => {
+    return (
+        <div className='miCuenta'>
+            <h2>Mi Cuenta</h2>
+            <div className='avatar'>
+                <Col xs={6} md={4}>
+                    <Image src="" alt='foto-perfil' roundedCircle />
+                </Col>
+            </div>
+            <div className='formulario'>
+                <Form>
+                    <Form.Group className='grupo' controlId="formBasicEmail">
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control type="text" placeholder='' disabled />
+                    </Form.Group>
+                    <Form.Group className='grupo' controlId="formBasicEmail">
+                        <Form.Label>Apellido</Form.Label>
+                        <Form.Control type="text" disabled />
+                    </Form.Group>
+                    <Form.Group className='grupo' controlId="formBasicEmail">
+                        <Form.Label>Edad</Form.Label>
+                        <Form.Control type="text" disabled />
+                    </Form.Group>
+                    <Form.Group className='grupo' controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" disabled />
+                    </Form.Group>
+                    <Form.Group className='grupo' controlId="formBasicEmail">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="text" disabled />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Editar
+                    </Button>
+                </Form>
+            </div>
+
+        </div>
+    )
+}
+
+export default CuentaUsuario

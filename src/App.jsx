965ef60c82231255f44f07ef87bb8ee3d4e681b1
Federico Valdiglesias <federicomrtl@gmail.com>
2023-06-08 19:45:23 -0300

    Se realizo una correcion en el routerPrincipal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react'
 import './App.css'
 import { RouterPrincipal } from './routers/RouterPrincipal'
 import "./components/layout/Header/header.css";

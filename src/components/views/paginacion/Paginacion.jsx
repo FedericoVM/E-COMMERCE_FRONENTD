a72a1ed1028c550dddc28e01712d82d9aec996ec
Federico Valdiglesias <federicomrtl@gmail.com>
2023-09-01 11:00:12 -0300

    Se cambio la estructura de los botones  de paginacion

--- src/components/views/paginacion/Paginacion.jsx
+++ src/components/views/paginacion/Paginacion.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
 
 
-const Paginacion = ({ postsPerPage, totalPosts, paginate }) => {
-    const pageNumbers = [];
-
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+  const pageNumbers = [];
+  
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
   return (
-    <nav className='mt-3'>
+    <div className='container d-flex justify-content-center'>
+    <nav className="d-flex justify-content-center mt-3">
     <ul className='pagination'>
-      {pageNumbers.map(number => (
-        <li key={number} className='page-item'>
-          <a onClick={() => paginate(number)} className='page-link'>
-            {number}
+    <li className='page-item' disabled={currentPage <= 1 ? true : false}>
+          <button onClick={() => paginate(currentPage - 1)}  disabled={currentPage <= 1 ? true : false} className='page-link'>
+            {"<"}
+          </button>
+        </li>
+        <li className={currentPage <= 1 ? 'd-none' : 'page-item'}>
+          <a onClick={() => paginate(currentPage - 1)} className='page-link'>
+            {currentPage - 1}
           </a>
         </li>
-      ))}
+        <li className='page-item'>
+          <a onClick={() => paginate(currentPage)} className={currentPage? 'active page-link':'page-link'}>
+            {currentPage}
+          </a>
+        </li>
+        <li className={currentPage >= pageNumbers.length ? 'd-none' : 'page-item'}>
+          <a onClick={() => paginate(currentPage + 1)} className='page-link'>
+            {currentPage + 1}
+          </a>
+        </li>
+        <li className='page-item' disabled={currentPage >= pageNumbers.length ? true : false}>
+          <button onClick={() => paginate(currentPage + 1)}  disabled={currentPage >= pageNumbers.length ? true : false} className='page-link'>
+            {">"}
+          </button>
+        </li>
     </ul>
   </nav>
+  </div>
   )
 }
 
-
-
-export default Paginacion
+export default Pagination
